d3adfbacffb99149944391d31646a224e1ce1d5e
Sifat Sultan <sifatsultan@gmail.com>
2022-05-25 18:00:00 +0600

    removed build from gitignore

---
+++ build/precache-manifest.caf5f1cdaa216abb24dbb1ca333ddb2b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b4591e80c2b0a17d4541f1ead3f2cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "246926a57366ee60143e",
+    "url": "/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "5d002890f934bbb9c48a",
+    "url": "/static/css/main.eda16212.chunk.css"
+  },
+  {
+    "revision": "246926a57366ee60143e",
+    "url": "/static/js/2.8a71eac6.chunk.js"
+  },
+  {
+    "revision": "8a76dff9c40135d870e44b235d8ac4e6",
+    "url": "/static/js/2.8a71eac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d002890f934bbb9c48a",
+    "url": "/static/js/main.f19f53b2.chunk.js"
+  },
+  {
+    "revision": "4005de77f30ef140aa53",
+    "url": "/static/js/runtime-main.c6082b8d.js"
+  },
+  {
+    "revision": "66e06d25eeee0f7cc7f609e1defb8957",
+    "url": "/static/media/bannerbackground1.66e06d25.jpg"
+  },
+  {
+    "revision": "092003ff8829a3665b2ee631efe908e9",
+    "url": "/static/media/logo_dominos.092003ff.png"
+  },
+  {
+    "revision": "c194ce1ff3fead68690fc69e169734ad",
+    "url": "/static/media/not_found.c194ce1f.jpg"
+  }
+]);
