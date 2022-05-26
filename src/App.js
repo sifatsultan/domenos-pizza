6a94c9058ce1170076e4162fa431355c9f275542
Sifat Sultan <sifatsultan@gmail.com>
2022-05-26 10:21:01 +0600

    fixed header state not accessible

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Header></Header>
         <Switch>
         <Route path="/home">
           <HomePage></HomePage>    
