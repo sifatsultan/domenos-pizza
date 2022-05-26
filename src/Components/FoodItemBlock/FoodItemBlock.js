1edf34e15a7c8e7e595885dff5511b3ea4135af2
Sifat Sultan <sifatsultan@gmail.com>
2022-05-26 10:33:20 +0600

    Cart item count reflecting in shopping cart icon

--- src/Components/FoodItemBlock/FoodItemBlock.js
+++ src/Components/FoodItemBlock/FoodItemBlock.js
@@ -1,19 +1,70 @@
 import React from 'react';
 import './FoodItemBlock.css';
-import { Link } from 'react-router-dom';
+import Modal from '@mui/material/Modal'
+import { Box } from '@mui/system';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import dummyData from '../../dummyData/foods';
+import { addToDatabaseCart } from '../../Database';
 
-const FoodItemBlock = (props) => {
+
+const FoodItemBlock = ({ food, addToCart }) => {
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+        setOpen(true);
+    }
+    const handleClose = () => setOpen(false);
+
+
+    const [quantity, setQuantity] = useState(1);
+    const totalPrice = quantity * food.price;
+
     return (
+
         <div className='container'>
-            <div style={{margin: '20px 0px'}}>
-            <Link to={'/fooditem/' + props.food.id}>
-            <div className="card food-item-block">
-                <img className="card-img-top" src={props.food.img} alt=""></img>
-                <h5>{props.food.name}</h5>
-                <p><small>{props.food.shortDescription}</small></p>
-                <h4>$<span>{props.food.price}</span></h4>
-            </div>
-            </Link>
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description">
+                <Box className="box-fooditem">
+                    <div className='item-details'>
+                        <div className="row">
+                            <div className="col-md-8">
+                                <h1>{food.name}</h1>
+                                <p>{food.fullDescription}</p>
+                                <div className="d-flex flex-row">
+                                    <h1 className='item-price'>${totalPrice}</h1>
+                                    <div className='food-quantity'>
+                                        <span className='btn' onClick={() => setQuantity(quantity <= 1 ? 1 : quantity - 1)}>-</span>
+                                        <b>{quantity}</b>
+                                        <span className='btn' onClick={() => setQuantity(quantity >= 20 ? 20 : quantity + 1)}>+</span>
+                                    </div>
+                                </div>
+                                <br />
+                                <button className='btn-add-to-cart' onClick={() => { addToDatabaseCart(food.id, quantity); addToCart() }}><FontAwesomeIcon icon={faCartPlus} /> Add</button>
+                            </div>
+                            <div className="col-md-4">
+                                <img src={food.img} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </Box>
+
+            </Modal>
+
+            <div style={{ margin: '20px 0px' }}>
+                <a onClick={handleOpen}>
+                    <div className="card food-item-block">
+                        <img className="card-img-top" src={food.img} alt=""></img>
+                        <h5>{food.name}</h5>
+                        <p><small>{food.shortDescription}</small></p>
+                        <h4>$<span>{food.price}</span></h4>
+                    </div>
+                </a>
             </div>
         </div>
     );
--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -3,29 +3,31 @@ import './HomePage.css'
 import dummyData from '../../dummyData/foods';
 import { useState } from 'react';
 import FoodItemBlock from '../FoodItemBlock/FoodItemBlock';
+import { getNumberOfCartItems } from '../../Database';
+import Header from '../Header/Header';
 
 
 const HomePage = () => {
     const [foodItem] = useState(dummyData);
     const [category, setCategory] = useState('vegeterian');
+    const [cartItemCount, setCartItemCount] = useState(getNumberOfCartItems())
+
+    const handleAddToCart = () => {
+        setCartItemCount(getNumberOfCartItems());
+    }
+    
     return (
         <div>
-            <div className="d-flex align-items-center justify-content-center flex-column homepage-banner">
-                <div className="p-2">
-                    <h1 className='banner-title'>Yummy and delicious pizzas to delight you!</h1>
-                </div>
-                <div className="p-2">
-                    <input className='search-food-item' type="text" placeholder='Search Food Items' />
-                    <button className='btn-search' >Search</button>
-                </div>
-            </div>
+            
+            <Header numberOfCartItems={cartItemCount}></Header>
+
 
             <div>
                 <ul className="nav justify-content-center menu-type">
-                    <li onClick={()=>setCategory('vegeterian')} className="nav-item">
+                    <li onClick={() => setCategory('vegeterian')} className="nav-item">
                         <h6 className={category === 'vegeterian' ? "active nav-link" : "nav-link"}>Vegeterian</h6>
                     </li>
-                    <li onClick={()=>setCategory('meat')} className="nav-item">
+                    <li onClick={() => setCategory('meat')} className="nav-item">
                         <h6 className={category === 'meat' ? "active nav-link" : "nav-link"}>Meat</h6>
                     </li>
                 </ul>
@@ -35,13 +37,13 @@ const HomePage = () => {
             <div className='food-items'>
                 {
                     foodItem.map((food) => {
-                    return(
-                        food.category === category &&
-                        <FoodItemBlock key={food.id} food={food}></FoodItemBlock>
-                    )
+                        return (
+                            food.category === category &&
+                            <FoodItemBlock key={food.id} food={food} addToCart={handleAddToCart}></FoodItemBlock>
+                        )
                     })
                 }
-                
+
             </div>
         </div>
     );
