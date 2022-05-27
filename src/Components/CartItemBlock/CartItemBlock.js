e4414fa3c36a266ad0ab0d7ca83420e0c7b112e6
Sifat Sultan <sifatsultan@gmail.com>
2022-05-27 15:44:37 +0600

    cart items syling improved

--- src/Components/CartItemBlock/CartItemBlock.js
+++ src/Components/CartItemBlock/CartItemBlock.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './CartItemBlock.css';
 
-const CartItemBlock = ({ food, addToCart, removeFromCart }) => {
+const CartItemBlock = ({ food,removeFromCart }) => {
 
     return (
         <div className='container'>
             <div style={{ margin: '20px 0px' }}>
                 <div className="card cart-item-block">
-                    <img className="card-img-top" src={food.img} alt=""></img>
-                    <h5>{food.name}</h5>
+                    <img className="card-img cart-item-img" src={food.img} alt=""></img>
+                    <h5 className='cart-item-name'>{food.name}</h5>
                     <h4>$<span>{food.price}</span></h4>
                     <button onClick={removeFromCart}>Remove</button>
                 </div>
