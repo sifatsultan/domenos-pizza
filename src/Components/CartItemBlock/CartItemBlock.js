a3fc8835bf9ab2313524d485e078a76eae27f682
Sifat Sultan <sifatsultan@gmail.com>
2022-05-28 12:08:33 +0600

    cart item added

--- src/Components/CartItemBlock/CartItemBlock.js
+++ src/Components/CartItemBlock/CartItemBlock.js
@@ -4,6 +4,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 import CloseIcon from '@mui/icons-material/Close';
 import { IconButton } from '@mui/material';
+import { ButtonGroup } from '@mui/material';
+import { Button } from '@mui/material';
 
 
 const CartItemBlock = ({ food, removeFromCart }) => {
@@ -11,21 +13,27 @@ const CartItemBlock = ({ food, removeFromCart }) => {
     return (
         <div className='container'>
             <div className="cart-item-block">
-                <div className='product-count-container'>
-                    <div className='food-quantity'>
-                        <span className='btn'>-</span>
-                        <b>{food.qty}</b>
-                        <span className='btn'>+</span>
-                    </div>
+                <div className='cart-item-container'>
+                    <ButtonGroup orientation='vertical' size="small" aria-label="vertical small outlined button group">
+                        <Button onClick={() => { console.log('Hello') }}>+</Button>
+                        <Button disabled>{food.qty}</Button>
+                        <Button onClick={() => { console.log('Bhai') }}>-</Button>
+                    </ButtonGroup>
                 </div>
-                <div className='cart-item-img-container'>
+                <div className='cart-item-img-container cart-item-container'>
                     <img className="cart-item-img" src={food.img} alt=""></img>
                 </div>
-                <p className='cart-item-name'>{food.name}</p>
-                <p>$<span>{food.price}</span></p>
-                <IconButton onClick={removeFromCart} >
-                    <CloseIcon />
-                </IconButton>
+                <div className='cart-item-container'>
+                    <p className='cart-item-name'>{food.name}</p>
+                </div>
+                <div className='cart-item-container'>
+                    <p>$<span>{food.price}</span></p>
+                </div>
+                <div className='cart-item-container'>
+                    <IconButton onClick={removeFromCart} >
+                        <CloseIcon />
+                    </IconButton>
+                </div>
             </div>
         </div>
     );
